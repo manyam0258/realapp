0e533770e6e8f6c22c1b9bf2d18d2b1b3ab3d4cd
manyam0258 <manyam.surendhranath@gmail.com>
2026-04-13 08:38:43 +0000

    remove tender settings from realapp and made it in tender module

--- realapp/realapp/doctype/tender_settings/tender_settings.js
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2026, Antigravity and contributors
-# For license information, please see license.txt
-
-frappe.ui.form.on('Tender Settings', {
-	refresh: function(frm) {
-
-	}
-});
--- realapp/realapp/doctype/tender_settings/tender_settings.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2026, Antigravity and contributors
-# For license information, please see license.txt
-
-import frappe
-from frappe.model.document import Document
-
-class TenderSettings(Document):
-	pass
