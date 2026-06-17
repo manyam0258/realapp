e28af423989ad111807eab9d6557006cabaf5030
manyam0258 <manyam.surendhranath@gmail.com>
2026-06-17 11:38:35 +0530

    Remove deprecated Tender Calendar doctype and report directories

--- realapp/tender/report/order_status_report/order_status_report.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026, surendhranath and contributors
-// For license information, please see license.txt
-
-frappe.query_reports["Order Status Report"] = {
-	"filters": [
-
-	]
-};
--- realapp/tender/report/quotation_status_report/quotation_status_report.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026, surendhranath and contributors
-// For license information, please see license.txt
-
-frappe.query_reports["Quotation Status Report"] = {
-	"filters": [
-
-	]
-};
